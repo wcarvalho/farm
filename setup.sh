1705cfd9797e25d918246d80ffa4f9ff6bfd00e9
Wilka <wcarvalh@umich.edu>
2022-04-11 13:34:07 -0400

    setup improved

--- setup.sh
+++ setup.sh
@@ -7,12 +7,12 @@ conda install -c anaconda cudnn==8.2.1 --force
 
 git clone https://github.com/deepmind/acme.git _acme
 cd _acme
-git checkout 6e1d71104371998e8cd0143cb8090c24263c50c4 3.0.0
+git checkout e7e99762369c2ab2871d1c4bc6b6ab776eddf48c # 4.0.0
 pip install --editable .[jax,tf,testing,envs]
 cd ..
 
-pip install --upgrade jax[cuda]==0.2.26 -f https://storage.googleapis.com/jax-releases/jax_releases.html
-# EXPECTED ERRORS for jax>=0.2.26
+pip install --upgrade jax[cuda]==0.2.27 -f https://storage.googleapis.com/jax-releases/jax_releases.html
+# EXPECTED ERRORS for jax>=0.2.27
 # 1. rlax 0.1.1 requires <=0.2.21
 # 2. distrax 0.1.0 requires jax<=0.2.21,
 
